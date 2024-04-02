568f6eedbbdc57c20beb000fac8871afd8b90fd7
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2024-04-02 19:20:54 -0300

    create a default template web app

---
+++ src/EventSchedulingPlatform.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EventSchedulingPlatform.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
